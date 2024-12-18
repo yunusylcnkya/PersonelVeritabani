d0ab043e7ca69e7a3bf6a235eef7a40615ee689a
yunus YALÇINKAYA <Yunusylcnkya@gmail.com>
2024-12-18 10:24:28 +0300

    UI Tasarımı Ve DatgridWiew SQL Bağlantısı Yapıldı Kaydet butonu programlandı

--- Veritabani/Form1.Designer.cs
+++ Veritabani/Form1.Designer.cs
@@ -305,6 +305,7 @@
             this.btnKaydet.TabIndex = 1;
             this.btnKaydet.Text = "Kaydet";
             this.btnKaydet.UseVisualStyleBackColor = true;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
             // 
             // btnSil
             // 
--- Veritabani/Form1.cs
+++ Veritabani/Form1.cs
@@ -7,15 +7,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Data.SqlClient;
 namespace Veritabani
 {
     public partial class FrmAnasayfa : Form
     {
+        SqlConnection baglanti = new SqlConnection(@"Data Source=DESKTOP-0R2ACEE\SQLEXPRESS01;
+        Initial Catalog=PersonelVeritabani;
+        Persist Security Info=True;User ID=sa; Password= 1234");
+
         public FrmAnasayfa()
         {
             InitializeComponent();
         }
+
 
         private void FrmAnasayfa_Load(object sender, EventArgs e)
         {
@@ -26,6 +31,22 @@ namespace Veritabani
         {
             this.tbl_PersonelTableAdapter.Fill(this.personelVeritabaniDataSet.Tbl_Personel);
 
+        }
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("insert into Tbl_Personel (PerAd,PerSoyad,PerSehir,PerMaas) " +
+                                                                        "values (@p1,@p2,@p3,@p4,@p5)", baglanti);
+
+            komut.Parameters.AddWithValue("@p1", txtAd.Text);
+            komut.Parameters.AddWithValue("@p2", txtSoyad.Text);
+            komut.Parameters.AddWithValue("@p3",cmbxSehir.Text);
+            komut.Parameters.AddWithValue("@p4",maskedTxtMaas.Text);
+            komut.Parameters.AddWithValue("@p5",txtMeslek.Text);
+            komut.ExecuteNonQuery();//insert ,Update ,Delete'de kullanılıyor
+            MessageBox.Show("Eklendi");
+            baglanti.Close();
         }
     }
 }
